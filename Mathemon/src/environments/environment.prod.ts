f7f91c41fddedca44e4cd417f0acd808d9b022d6
ThatOnePandaGuy <noahwblumenfeld@gmail.com>
2018-02-20 18:39:15 -0700

    bootstrap dependecy

--- Mathemon/src/environments/environment.prod.ts
+++ Mathemon/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDmOGENgk2kp_d6Bre9pkfS-ortP_beqBw",
+    authDomain: "mathemon-bacc4.firebaseapp.com",
+    databaseURL: "https://mathemon-bacc4.firebaseio.com",
+    projectId: "mathemon-bacc4",
+    storageBucket: "",
+    messagingSenderId: "804861227648"
+  }
 };
