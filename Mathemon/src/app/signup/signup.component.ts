b6ecc496d8dfca4864c62c89ef23897752d94322
ThatOnePandaGuy <noahwblumenfeld@gmail.com>
2018-02-22 20:24:15 -0700

    working on form stuff

--- Mathemon/src/app/signup/signup.component.ts
+++ Mathemon/src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-signup',
@@ -6,13 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-  click() {
-    alert("clicked");
-  }
-  constructor() { }
 
+  private signupForm: AngularFirestoreDocument<FormData>
+  item:Observable<FormData>
+  teacherCollectionReference;
+
+  @Input()
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+
+  constructor(private afs: AngularFirestore) { 
+    this.teacherCollectionReference = this.afs.collection('Teachers');
+  }
+  
   ngOnInit() {
   }
   
+
+  submitData():void {
+    this.teacherCollectionReference.add({email:this.email,name: {first_name:this.firstName, last_name:this.lastName}, password:this.password});
+  }
+
+  click() {
+    alert("clicked");
+  }
 
 }
