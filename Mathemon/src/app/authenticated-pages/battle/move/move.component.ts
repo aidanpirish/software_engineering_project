6343d2568c6a78e356722a2255feced35e825c9e
ThatOnePandaGuy <noahwblumenfeld@gmail.com>
2018-04-03 20:29:00 -0600

    fixed some stuff

--- Mathemon/src/app/authenticated-pages/battle/move/move.component.ts
+++ Mathemon/src/app/authenticated-pages/battle/move/move.component.ts
@@ -8,6 +8,10 @@ import { problem } from '../../../interfaces/problem.interface';
 })
 export class MoveComponent implements OnInit {
 
+  @Input() difficulty: string;
+  @Input() move: string;
+  @Input() type: string;
+
   @Input() problemNumber: number;
   @Output() change = new EventEmitter();
   constructor() { }
