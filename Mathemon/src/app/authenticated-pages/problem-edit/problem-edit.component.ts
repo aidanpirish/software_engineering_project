18bb29e51610441bed81d52392d33a18c62a3436
Aidan Irish <aidanpirish@gmail.com>
2018-04-24 19:11:47 -0600

    problem edit

--- Mathemon/src/app/authenticated-pages/problem-edit/problem-edit.component.ts
+++ Mathemon/src/app/authenticated-pages/problem-edit/problem-edit.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class ProblemEditComponent implements OnInit {
 
-  problems:Problem[];
+  //problems:Problem[];
 
   constructor(private global: GlobalService, private router: Router) {
    }
